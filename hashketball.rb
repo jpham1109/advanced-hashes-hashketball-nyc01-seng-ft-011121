75a1728fdb359fbb4ae4be3fa5d682aa202eabe1
Jamie Lawrence-Hurt <jpham1109@gmail.com>
2021-01-07 00:04:55 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,6 +130,7 @@ def num_points_scored(player_name)
   result = ""
   game_hash.each do |loc, team|
     game_hash[loc].each do |team_info|
+      binding.pry
       game_hash[loc][team_info].each do |players|
         if player_name == game_hash[loc][team_info][players][player_name]
           result = game_hash[loc][team_info][players][points]
